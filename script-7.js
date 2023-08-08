c7984d24e22abddf009eca8b82ca4a631485ad86
Heyabhiyadav <141588193+Heyabhiyadav@users.noreply.github.com>
2023-08-09 02:16:53 +0530

    Add files via upload

---
+++ script-7.js
@@ -0,0 +1,8 @@
+const menuBarEl = document.getElementById("menuBar");
+const menuMobileEl = document.querySelector('.menu_mobile');
+const onClose = () => {
+	menuMobileEl.classList.toggle('show_menu');
+};
+menuBarEl.addEventListener("click",() => {
+	onClose();
+});
